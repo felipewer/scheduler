e232c625926e3e812b0c29589dd112dee1c41827
Felipe Carlos Werlang <felipewer@gmail.com>
2018-08-15 16:01:35 -0300

    Finish date service tests

--- test/services/date.unit.test.ts
+++ test/services/date.unit.test.ts
@@ -1,5 +1,5 @@
 import moment from "moment-timezone";
-import { getMomentAt } from '../../src/services/date';
+import { getMomentAt, nextAvailableTime } from '../../src/services/date';
 
 
 describe('Get moment at specific date, hour and timezone', () => {
@@ -36,47 +36,66 @@ describe('Get next available time', () => {
 
   test('Should return a moment 4 hours in the future', () => {
     const now = moment('2018-08-14').tz('America/Sao_Paulo').startOf('day');
-    const nextTime = now.clone().add(4, 'hours');
+    const events = new Map<string,Set<string>>();
+    const minHour = '00:00:00-03:00';
+    const maxHour = '22:00:00-03:00';
+    const nextTime = nextAvailableTime(events, minHour, maxHour, now);
     expect(nextTime.diff(now, 'hours')).toBe(4);
   });
 
   test('Should return a moment >= the minimum hour', () => {
     const { hour, timezone } = { hour: 7, timezone: 'America/Sao_Paulo'};
     const now = moment('2018-08-14').tz(timezone).startOf('day');
-    const nextTime = now.clone().add(hour, 'hours');
+    const events = new Map<string,Set<string>>();
+    const minHour = '07:00:00-03:00';
+    const maxHour = '22:00:00-03:00';
+    const nextTime = nextAvailableTime(events, minHour, maxHour, now);
     expect(nextTime.isSameOrAfter('2018-08-14T07:00:00-03:00')).toBeTruthy();
   });
 
   test('Should return a moment < the maximum hour', () => {
     const { hour, timezone } = { hour: 22, timezone: 'America/Sao_Paulo'};
     const now = moment('2018-08-14').tz(timezone).startOf('day');
-    const nextTime = now.clone().add(hour - 1, 'hours');
+    const events = new Map<string,Set<string>>();
+    const minHour = '07:00:00-03:00';
+    const maxHour = '22:00:00-03:00';
+    const nextTime = nextAvailableTime(events, minHour, maxHour, now);
     expect(nextTime.isBefore('2018-08-14T22:00:00-03:00')).toBeTruthy();
   });
 
   test('Should return a moment >= the minimum hour on the next weekday', () => {
     const { hour, timezone } = { hour: 7, timezone: 'America/Sao_Paulo'};
     const now = moment('2018-08-18').tz(timezone).startOf('day');
-    const nextTime = now.clone().add(2, 'days').add(hour, 'hours');
+    const events = new Map<string,Set<string>>();
+    const minHour = '07:00:00-03:00';
+    const maxHour = '22:00:00-03:00';
+    const nextTime = nextAvailableTime(events, minHour, maxHour, now);
     expect(nextTime.isSameOrAfter('2018-08-20T07:00:00-03:00')).toBeTruthy();
   });
 
   test('Should return a moment < the maximum hour on the next weekday', () => {
     const { hour, timezone } = { hour: 22, timezone: 'America/Sao_Paulo'};
     const now = moment('2018-08-17').tz(timezone).startOf('day');
-    const nextTime = now.clone().add(3, 'days').add(hour - 1, 'hours');
+    const events = new Map<string,Set<string>>();
+    const minHour = '07:00:00-03:00';
+    const maxHour = '22:00:00-03:00';
+    const nextTime = nextAvailableTime(events, minHour, maxHour, now);
     expect(nextTime.isBefore('2018-08-20T22:00:00-03:00')).toBeTruthy();
   });
 
   test('Should return a moment not already taken', () => {
     const { hour, timezone } = { hour: 7, timezone: 'America/Sao_Paulo'};
     const taken = new Set<string>([
-      '2018-08-14T07:00:00-03:00',
-      '2018-08-14T08:00:00-03:00',
-      '2018-08-14T10:00:00-03:00'
+      '2018-08-14T11:00:00-03:00',
+      '2018-08-14T12:00:00-03:00',
+      '2018-08-14T14:00:00-03:00'
     ]);
     const now = moment('2018-08-14').tz(timezone).hours(hour).startOf('hour');
-    const nextTime = now.clone().add(2, 'hours');
+    const events = new Map<string,Set<string>>();
+    events.set('2018-08-14', taken);
+    const minHour = '07:00:00-03:00';
+    const maxHour = '22:00:00-03:00';
+    const nextTime = nextAvailableTime(events, minHour, maxHour, now);
     expect(taken.has(nextTime.format())).toBeFalsy();
     expect(taken.has(nextTime.clone().subtract(1, 'h').format())).toBeTruthy();
     expect(taken.has(nextTime.clone().add(1, 'h').format())).toBeTruthy();
@@ -90,7 +109,11 @@ describe('Get next available time', () => {
       '2018-08-17T21:00:00-03:00'
     ]);
     const now = moment('2018-08-17').tz(timezone).hours(hour).startOf('hour');
-    const nextTime = now.clone().add(3, 'days');
+    const events = new Map<string,Set<string>>();
+    events.set('2018-08-17', taken);
+    const minHour = '07:00:00-03:00';
+    const maxHour = '22:00:00-03:00';
+    const nextTime = nextAvailableTime(events, minHour, maxHour, now);
     expect(nextTime.isSameOrAfter('2018-08-20T07:00:00-03:00')).toBeTruthy();
     expect(nextTime.isBefore('2018-08-20T22:00:00-03:00')).toBeTruthy();
     expect(taken.has(nextTime.format())).toBeFalsy();
