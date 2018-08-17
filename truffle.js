dec7e60aa6c1491fe7a3bd50c4b1fa76a74c878a
Felipe Carlos Werlang <felipewer@gmail.com>
2018-08-17 17:46:25 -0300

    Deploy contract to Ropsten network

--- truffle.js
+++ truffle.js
@@ -11,6 +11,12 @@
  *     gasPrice: 10000000000,
  *   },
  */
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+require('dotenv').config();
+
+const mnemonic = process.env.HDW_MNEMONIC;
+const infuraKey = process.env.INFURA_API_KEY
 
 module.exports = {
   networks: {
@@ -18,6 +24,15 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    ropsten: {
+      provider: function() { 
+        return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`) 
+      },
+      network_id: '3',
+      gas: 4500000,
+      gasPrice: 10000000000,
+    },
+   
   }
 };
